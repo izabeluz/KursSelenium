c8d0afa846d44c465b4ddce8459426fe66491f08
Izabela Olejnik <izabelapawlak87@gmail.com>
2019-10-14 17:15:20 +0200

    Bing zadanie

---
+++ src/test/java/BingSearch.java
@@ -0,0 +1,45 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BingSearch {
+
+    private WebDriver driver;//definiujemy zmienna driver
+
+    @Before//czesc skladowa Junit, funkcja i mowi ze ma to byc na poczatku
+    public void setUp() { //deklarujemy funkcje setup
+        System.setProperty("webdriver.chrome.driver",
+                "src/main/resources/chromedriver.exe");
+        // Uruchom nowy egzemplarz przeglądarki Chrome
+        driver = new ChromeDriver();
+
+        // Zmaksymalizuj okno przeglądarki
+        driver.manage().window().maximize();
+        // Przejdź do Bing
+        driver.get("https://www.bing.com/");
+    }
+
+    @Test//metoda Junit test
+    public void testBingSearch() {
+        // Znajdź element wprowadzania tekstu na podstawie jego nazwy
+        WebElement element = driver.findElement(By.name("q"));
+        // Wyczyść teskst zapisany w elemencie
+        element.clear();
+
+        // Wpisz informacje do wyszukania
+        element.sendKeys("Mistrzostwa Swiata w piłce nożnej 2018 ");
+
+        // Prześlij formularz
+        element.submit();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        // Zamknij przeglądarkę
+        driver.quit();
+    }
+}
